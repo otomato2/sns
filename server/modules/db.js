37758f71d0f70960564297bf0f155bfad5943ecf
otomato2 <yukiyoshi0001U@gmail.com>
2024-02-28 05:04:37 +0900

    third commit

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const neo4j = require('neo4j-driver');
-const driver = neo4j.driver('bolt://172.21.219.37:7687', neo4j.auth.basic('neo4j', 'helloworld'));
+const driver = neo4j.driver(`bolt://${process.env.NEO4J_IP_ADDR}:7687`, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD));
+
+console.log(driver)
 
 const session = driver.session();
 
